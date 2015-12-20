98fda1b92729575216a90a69e3e186f7e2b1748c
praveen <praveen.shastrula@gmail.com>
2015-12-20 00:45:51 -0600

    Kitchen Book for Ubuntu and AWS for installing tomcat and example spring boot test war

---
+++ cookbooks/aws-cookbook/recipes/default.rb
@@ -0,0 +1,35 @@
+package 'tomcat7' do
+  action :install
+end
+
+package 'tomcat7-webapps' do
+  action :install
+end
+
+package 'tomcat7-admin-webapps' do
+  action :install
+end
+
+template '/usr/share/tomcat7/conf/tomcat-users.xml' do
+  source 'tomcat-users.xml.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+service "tomcat7" do
+  action :enable
+end
+
+service "tomcat7" do
+  action :restart
+end
+
+remote_file '/usr/share/tomcat7/webapps/spring-boot.war' do
+  source 'https://repo1.maven.org/maven2/org/springframework/boot/spring-boot-deployment-test-tomcat/1.3.1.RELEASE/spring-boot-deployment-test-tomcat-1.3.1.RELEASE.war'
+  action :create
+end
+
+service "tomcat7" do
+  action :restart
+end
